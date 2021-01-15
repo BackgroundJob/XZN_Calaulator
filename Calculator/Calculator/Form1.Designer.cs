e0d89f72e093f393dd83096c36786f02118f952c
计算机17谢振宁 <57025974+padio995@users.noreply.github.com>
2021-01-15 19:55:38 +0800

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_sub = new System.Windows.Forms.Button();
+            this.button_result = new System.Windows.Forms.Button();
+            this.button_mul = new System.Windows.Forms.Button();
+            this.button_div = new System.Windows.Forms.Button();
+            this.button_reset = new System.Windows.Forms.Button();
+            this.info = new System.Windows.Forms.Label();
+            this.button_power = new System.Windows.Forms.Button();
+            this.button_square = new System.Windows.Forms.Button();
+            this.button_antipow = new System.Windows.Forms.Button();
+            this.time = new System.Windows.Forms.Button();
+            this.sin = new System.Windows.Forms.Button();
+            this.cos = new System.Windows.Forms.Button();
+            this.button_Binary = new System.Windows.Forms.Button();
+            this.button_hex = new System.Windows.Forms.Button();
+            this.button_Ln = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("宋体", 20F);
+            this.textBox.Location = new System.Drawing.Point(46, 34);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(495, 46);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "input num";
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(64, 137);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(75, 23);
+            this.button_add.TabIndex = 1;
+            this.button_add.Text = "+";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_sub
+            // 
+            this.button_sub.Location = new System.Drawing.Point(197, 137);
+            this.button_sub.Name = "button_sub";
+            this.button_sub.Size = new System.Drawing.Size(75, 23);
+            this.button_sub.TabIndex = 2;
+            this.button_sub.Text = "-";
+            this.button_sub.UseVisualStyleBackColor = true;
+            this.button_sub.Click += new System.EventHandler(this.button_sub_Click);
+            // 
+            // button_result
+            // 
+            this.button_result.Location = new System.Drawing.Point(64, 333);
+            this.button_result.Name = "button_result";
+            this.button_result.Size = new System.Drawing.Size(75, 23);
+            this.button_result.TabIndex = 3;
+            this.button_result.Text = "=";
+            this.button_result.UseVisualStyleBackColor = true;
+            this.button_result.Click += new System.EventHandler(this.button_result_Click);
+            // 
+            // button_mul
+            // 
+            this.button_mul.Location = new System.Drawing.Point(64, 212);
+            this.button_mul.Name = "button_mul";
+            this.button_mul.Size = new System.Drawing.Size(75, 23);
+            this.button_mul.TabIndex = 4;
+            this.button_mul.Text = "x";
+            this.button_mul.UseVisualStyleBackColor = true;
+            this.button_mul.Click += new System.EventHandler(this.button_mul_Click);
+            // 
+            // button_div
+            // 
+            this.button_div.Location = new System.Drawing.Point(197, 212);
+            this.button_div.Name = "button_div";
+            this.button_div.Size = new System.Drawing.Size(75, 23);
+            this.button_div.TabIndex = 5;
+            this.button_div.Text = "÷";
+            this.button_div.UseVisualStyleBackColor = true;
+            this.button_div.Click += new System.EventHandler(this.button_div_Click);
+            // 
+            // button_reset
+            // 
+            this.button_reset.Location = new System.Drawing.Point(197, 333);
+            this.button_reset.Name = "button_reset";
+            this.button_reset.Size = new System.Drawing.Size(75, 23);
+            this.button_reset.TabIndex = 7;
+            this.button_reset.Text = "reset";
+            this.button_reset.UseVisualStyleBackColor = true;
+            this.button_reset.Click += new System.EventHandler(this.button_reset_Click);
+            // 
+            // info
+            // 
+            this.info.AutoSize = true;
+            this.info.Location = new System.Drawing.Point(46, 87);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(142, 15);
+            this.info.TabIndex = 8;
+            this.info.Text = "请输入第一个运算数";
+            // 
+            // button_power
+            // 
+            this.button_power.Location = new System.Drawing.Point(335, 137);
+            this.button_power.Name = "button_power";
+            this.button_power.Size = new System.Drawing.Size(75, 23);
+            this.button_power.TabIndex = 9;
+            this.button_power.Text = "x^n";
+            this.button_power.UseVisualStyleBackColor = true;
+            this.button_power.Click += new System.EventHandler(this.button_power_Click);
+            // 
+            // button_square
+            // 
+            this.button_square.Location = new System.Drawing.Point(466, 137);
+            this.button_square.Name = "button_square";
+            this.button_square.Size = new System.Drawing.Size(75, 23);
+            this.button_square.TabIndex = 10;
+            this.button_square.Text = "x^2";
+            this.button_square.UseVisualStyleBackColor = true;
+            this.button_square.Click += new System.EventHandler(this.button_square_Click);
+            // 
+            // button_antipow
+            // 
+            this.button_antipow.Location = new System.Drawing.Point(335, 212);
+            this.button_antipow.Name = "button_antipow";
+            this.button_antipow.Size = new System.Drawing.Size(75, 23);
+            this.button_antipow.TabIndex = 11;
+            this.button_antipow.Text = "n√";
+            this.button_antipow.UseVisualStyleBackColor = true;
+            this.button_antipow.Click += new System.EventHandler(this.button_antipow_Click);
+            // 
+            // time
+            // 
+            this.time.Location = new System.Drawing.Point(335, 333);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(75, 23);
+            this.time.TabIndex = 12;
+            this.time.Text = "time";
+            this.time.UseVisualStyleBackColor = true;
+            this.time.Click += new System.EventHandler(this.time_Click);
+            // 
+            // sin
+            // 
+            this.sin.Location = new System.Drawing.Point(335, 273);
+            this.sin.Name = "sin";
+            this.sin.Size = new System.Drawing.Size(75, 23);
+            this.sin.TabIndex = 13;
+            this.sin.Text = "sin";
+            this.sin.UseVisualStyleBackColor = true;
+            this.sin.Click += new System.EventHandler(this.sin_Click);
+            // 
+            // cos
+            // 
+            this.cos.Location = new System.Drawing.Point(466, 273);
+            this.cos.Name = "cos";
+            this.cos.Size = new System.Drawing.Size(75, 23);
+            this.cos.TabIndex = 14;
+            this.cos.Text = "cos";
+            this.cos.UseVisualStyleBackColor = true;
+            this.cos.Click += new System.EventHandler(this.cos_Click);
+            // 
+            // button_Binary
+            // 
+            this.button_Binary.Font = new System.Drawing.Font("宋体", 7F);
+            this.button_Binary.Location = new System.Drawing.Point(64, 273);
+            this.button_Binary.Name = "button_Binary";
+            this.button_Binary.Size = new System.Drawing.Size(75, 23);
+            this.button_Binary.TabIndex = 15;
+            this.button_Binary.Text = "十转二";
+            this.button_Binary.UseVisualStyleBackColor = true;
+            this.button_Binary.Click += new System.EventHandler(this.button_Binary_Click);
+            // 
+            // button_hex
+            // 
+            this.button_hex.Font = new System.Drawing.Font("宋体", 7F);
+            this.button_hex.Location = new System.Drawing.Point(197, 273);
+            this.button_hex.Name = "button_hex";
+            this.button_hex.Size = new System.Drawing.Size(75, 23);
+            this.button_hex.TabIndex = 16;
+            this.button_hex.Text = "十转十六";
+            this.button_hex.UseVisualStyleBackColor = true;
+            this.button_hex.Click += new System.EventHandler(this.button_hex_Click);
+            // 
+            // button_Ln
+            // 
+            this.button_Ln.Location = new System.Drawing.Point(466, 212);
+            this.button_Ln.Name = "button_Ln";
+            this.button_Ln.Size = new System.Drawing.Size(75, 23);
+            this.button_Ln.TabIndex = 17;
+            this.button_Ln.Text = "ln";
+            this.button_Ln.UseVisualStyleBackColor = true;
+            this.button_Ln.Click += new System.EventHandler(this.button_Ln_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Ln);
+            this.Controls.Add(this.button_hex);
+            this.Controls.Add(this.button_Binary);
+            this.Controls.Add(this.cos);
+            this.Controls.Add(this.sin);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.button_antipow);
+            this.Controls.Add(this.button_square);
+            this.Controls.Add(this.button_power);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.button_reset);
+            this.Controls.Add(this.button_div);
+            this.Controls.Add(this.button_mul);
+            this.Controls.Add(this.button_result);
+            this.Controls.Add(this.button_sub);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_sub;
+        private System.Windows.Forms.Button button_result;
+        private System.Windows.Forms.Button button_mul;
+        private System.Windows.Forms.Button button_div;
+        private System.Windows.Forms.Button button_reset;
+        private System.Windows.Forms.Label info;
+        private System.Windows.Forms.Button button_power;
+        private System.Windows.Forms.Button button_square;
+        private System.Windows.Forms.Button button_antipow;
+        private System.Windows.Forms.Button time;
+        private System.Windows.Forms.Button sin;
+        private System.Windows.Forms.Button cos;
+        private System.Windows.Forms.Button button_Binary;
+        private System.Windows.Forms.Button button_hex;
+        private System.Windows.Forms.Button button_Ln;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double number1=0;
+        double number2;
+        double result;
+        bool Is_sign_input=false;
+        string sign;
+        System.Timers.Timer myTimer;
+        bool timer_start=false;
+
+
+        public bool checknum (String value)//判断输入的字符串是否为一个合理的数字
+        {
+            bool result = Regex.IsMatch(value, @"^(-?[0-9]*[.]*[0-9]{0,3})$");
+            return result ;
+
+        }
+
+        public void operation(String op_sign)//确定运算符号的方法
+        {
+            stop_timer();
+            if (checknum(textBox.Text))
+            {
+                number1 = double.Parse(textBox.Text);
+                textBox.Text = "";
+                Is_sign_input = true;
+                sign = op_sign;
+                info.Text = "请输入第二个运算数";
+            }
+            else
+            {
+                info.Text = "请输入有效数字";
+            }
+           
+        }
+        public void reset()//除上一次的计算结果外全部重置的方法
+        {
+            number1 = 0;
+            number2 = 0;
+            Is_sign_input = false;
+            sign = "";
+        }
+
+    private void button_add_Click(object sender, EventArgs e)
+        {
+            operation("add");
+        }
+
+        private void button_sub_Click(object sender, EventArgs e)
+        {
+            operation("sub");
+        }
+
+        private void button_mul_Click(object sender, EventArgs e)
+        {
+            operation("mul");
+        }
+
+        private void button_div_Click(object sender, EventArgs e)
+        {
+            operation("div");
+        }
+
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            stop_timer();
+            if (Is_sign_input && checknum(textBox.Text))
+            {
+                number2 = double.Parse(textBox.Text);
+                switch (sign)
+                {
+                    case "add" :result = number1 + number2;break;
+                    case "sub": result = number1 - number2; break;
+                    case "mul": result = number1 * number2; break;
+                    case "div":
+                        {
+                            if (number2 != 0)
+                                result = number1/number2;
+                            else
+                                info.Text = "被除数不能为0！";
+                        }
+                        break;
+                    case "power":result = Math.Pow(number1, number2);break;
+                    case "antipow":result = Math.Pow(number1, (1 / number2));break;
+                       
+                }
+                textBox.Text = Convert.ToString(result);
+                reset();
+            }
+            else
+            {
+                if (Is_sign_input)
+                {
+                    info.Text = "还未输入选择运算方式！";
+                }
+            }
+
+        }
+
+        private void button_reset_Click(object sender, EventArgs e)
+        {
+            reset();
+            result = 0;
+            textBox.Text = "";
+            info.Text = "请输入第一个运算数";
+        }
+
+        private void button_power_Click(object sender, EventArgs e)
+        {
+            operation("power");
+        }
+
+        private void button_square_Click(object sender, EventArgs e)
+        {
+            stop_timer();
+            if (checknum(textBox.Text))
+            {
+                number1 = double.Parse(textBox.Text);
+                result = number1 * number1;
+                textBox.Text = Convert.ToString(result);
+                reset();
+            }
+        }
+
+        private void button_antipow_Click(object sender, EventArgs e)
+        {
+            operation("antipow");
+        }
+
+        private void time_Click(object sender, EventArgs e)
+        {
+            timer_start = true;
+            myTimer = new System.Timers.Timer(1000);//定时周期2秒
+            myTimer.Elapsed += reset_time;//到1秒了做的事件
+            myTimer.AutoReset = true; //是否不断重复定时器操作
+            myTimer.Enabled = true;
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+        }
+        public void reset_time(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            info.Text = "当前时间：" + Convert.ToString(DateTime.Now);
+        }
+        public void stop_timer()
+        {
+            if (timer_start)
+            {
+                myTimer.Close(); //释放Timer占用的资源
+                myTimer.Dispose();
+            }
+            
+        }
+
+        private void sin_Click(object sender, EventArgs e)
+        {
+            stop_timer();
+            if (checknum(textBox.Text))
+            {
+                number1 = double.Parse(textBox.Text);
+                if (number1 > 360||number1 < 0){
+                    info.Text="请输入合适的角度";
+                }
+                else
+                {
+                    double RadianValue = Math.PI * number1 / 180;
+                    result = Math.Sin(RadianValue);
+                    textBox.Text = Convert.ToString(result);
+                    reset();
+                }
+               
+            }
+
+        }
+
+        private void cos_Click(object sender, EventArgs e)
+        {
+            stop_timer();
+            if (checknum(textBox.Text))
+            {
+                number1 = double.Parse(textBox.Text);
+                if (number1 > 360 || number1 < 0)
+                {
+                    info.Text = "请输入合适的角度";
+                }
+                else
+                {
+                    double RadianValue = Math.PI * number1 / 180;
+                    result = Math.Cos(RadianValue);
+                    textBox.Text = Convert.ToString(result);
+                    reset();
+                }
+
+            }
+        }
+
+        private void button_Binary_Click(object sender, EventArgs e)
+        {
+            stop_timer();
+            if (checknum(textBox.Text))
+            {
+                number1 = double.Parse(textBox.Text);
+                int a = (int)number1;
+                textBox.Text = Convert.ToString(a,2);
+                result = double.Parse(textBox.Text);
+                
+            }
+
+        }
+
+        private void button_hex_Click(object sender, EventArgs e)
+        {
+            stop_timer();
+            if (checknum(textBox.Text))
+            {
+                number1 = double.Parse(textBox.Text);
+                int a = (int)number1;
+                textBox.Text = Convert.ToString(a, 16);
+                result = double.Parse(textBox.Text);
+               
+            }
+        }
+
+        private void button_Ln_Click(object sender, EventArgs e)
+        {
+            stop_timer();
+            if (checknum(textBox.Text))
+            {
+                number1 = double.Parse(textBox.Text);
+                result = Math.Log(number1);
+                textBox.Text = Convert.ToString(result);
+                reset();
+            }
+        }
+    }
+}
