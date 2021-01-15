94483e1fc6e93f92b0a36d6cf29ce3bf7e3951b7
计算机17谢振宁 <57025974+padio995@users.noreply.github.com>
2021-01-15 20:18:38 +0800

    Add files via upload

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -59,9 +59,10 @@ namespace Calculator
             // 
             // button_add
             // 
-            this.button_add.Location = new System.Drawing.Point(64, 137);
+            this.button_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_add.Location = new System.Drawing.Point(46, 114);
             this.button_add.Name = "button_add";
-            this.button_add.Size = new System.Drawing.Size(75, 23);
+            this.button_add.Size = new System.Drawing.Size(124, 69);
             this.button_add.TabIndex = 1;
             this.button_add.Text = "+";
             this.button_add.UseVisualStyleBackColor = true;
@@ -69,9 +70,10 @@ namespace Calculator
             // 
             // button_sub
             // 
-            this.button_sub.Location = new System.Drawing.Point(197, 137);
+            this.button_sub.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_sub.Location = new System.Drawing.Point(166, 114);
             this.button_sub.Name = "button_sub";
-            this.button_sub.Size = new System.Drawing.Size(75, 23);
+            this.button_sub.Size = new System.Drawing.Size(124, 69);
             this.button_sub.TabIndex = 2;
             this.button_sub.Text = "-";
             this.button_sub.UseVisualStyleBackColor = true;
@@ -79,9 +81,10 @@ namespace Calculator
             // 
             // button_result
             // 
-            this.button_result.Location = new System.Drawing.Point(64, 333);
+            this.button_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_result.Location = new System.Drawing.Point(46, 306);
             this.button_result.Name = "button_result";
-            this.button_result.Size = new System.Drawing.Size(75, 23);
+            this.button_result.Size = new System.Drawing.Size(124, 69);
             this.button_result.TabIndex = 3;
             this.button_result.Text = "=";
             this.button_result.UseVisualStyleBackColor = true;
@@ -89,9 +92,10 @@ namespace Calculator
             // 
             // button_mul
             // 
-            this.button_mul.Location = new System.Drawing.Point(64, 212);
+            this.button_mul.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_mul.Location = new System.Drawing.Point(46, 178);
             this.button_mul.Name = "button_mul";
-            this.button_mul.Size = new System.Drawing.Size(75, 23);
+            this.button_mul.Size = new System.Drawing.Size(124, 69);
             this.button_mul.TabIndex = 4;
             this.button_mul.Text = "x";
             this.button_mul.UseVisualStyleBackColor = true;
@@ -99,9 +103,10 @@ namespace Calculator
             // 
             // button_div
             // 
-            this.button_div.Location = new System.Drawing.Point(197, 212);
+            this.button_div.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_div.Location = new System.Drawing.Point(166, 178);
             this.button_div.Name = "button_div";
-            this.button_div.Size = new System.Drawing.Size(75, 23);
+            this.button_div.Size = new System.Drawing.Size(124, 69);
             this.button_div.TabIndex = 5;
             this.button_div.Text = "÷";
             this.button_div.UseVisualStyleBackColor = true;
@@ -109,9 +114,10 @@ namespace Calculator
             // 
             // button_reset
             // 
-            this.button_reset.Location = new System.Drawing.Point(197, 333);
+            this.button_reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_reset.Location = new System.Drawing.Point(166, 306);
             this.button_reset.Name = "button_reset";
-            this.button_reset.Size = new System.Drawing.Size(75, 23);
+            this.button_reset.Size = new System.Drawing.Size(124, 69);
             this.button_reset.TabIndex = 7;
             this.button_reset.Text = "reset";
             this.button_reset.UseVisualStyleBackColor = true;
@@ -120,17 +126,19 @@ namespace Calculator
             // info
             // 
             this.info.AutoSize = true;
+            this.info.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.info.Location = new System.Drawing.Point(46, 87);
             this.info.Name = "info";
-            this.info.Size = new System.Drawing.Size(142, 15);
+            this.info.Size = new System.Drawing.Size(134, 17);
             this.info.TabIndex = 8;
             this.info.Text = "请输入第一个运算数";
             // 
             // button_power
             // 
-            this.button_power.Location = new System.Drawing.Point(335, 137);
+            this.button_power.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_power.Location = new System.Drawing.Point(286, 114);
             this.button_power.Name = "button_power";
-            this.button_power.Size = new System.Drawing.Size(75, 23);
+            this.button_power.Size = new System.Drawing.Size(124, 69);
             this.button_power.TabIndex = 9;
             this.button_power.Text = "x^n";
             this.button_power.UseVisualStyleBackColor = true;
@@ -138,9 +146,10 @@ namespace Calculator
             // 
             // button_square
             // 
-            this.button_square.Location = new System.Drawing.Point(466, 137);
+            this.button_square.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_square.Location = new System.Drawing.Point(406, 114);
             this.button_square.Name = "button_square";
-            this.button_square.Size = new System.Drawing.Size(75, 23);
+            this.button_square.Size = new System.Drawing.Size(124, 69);
             this.button_square.TabIndex = 10;
             this.button_square.Text = "x^2";
             this.button_square.UseVisualStyleBackColor = true;
@@ -148,9 +157,10 @@ namespace Calculator
             // 
             // button_antipow
             // 
-            this.button_antipow.Location = new System.Drawing.Point(335, 212);
+            this.button_antipow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_antipow.Location = new System.Drawing.Point(286, 178);
             this.button_antipow.Name = "button_antipow";
-            this.button_antipow.Size = new System.Drawing.Size(75, 23);
+            this.button_antipow.Size = new System.Drawing.Size(124, 69);
             this.button_antipow.TabIndex = 11;
             this.button_antipow.Text = "n√";
             this.button_antipow.UseVisualStyleBackColor = true;
@@ -158,9 +168,10 @@ namespace Calculator
             // 
             // time
             // 
-            this.time.Location = new System.Drawing.Point(335, 333);
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.Location = new System.Drawing.Point(286, 306);
             this.time.Name = "time";
-            this.time.Size = new System.Drawing.Size(75, 23);
+            this.time.Size = new System.Drawing.Size(124, 69);
             this.time.TabIndex = 12;
             this.time.Text = "time";
             this.time.UseVisualStyleBackColor = true;
@@ -168,9 +179,10 @@ namespace Calculator
             // 
             // sin
             // 
-            this.sin.Location = new System.Drawing.Point(335, 273);
+            this.sin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sin.Location = new System.Drawing.Point(286, 242);
             this.sin.Name = "sin";
-            this.sin.Size = new System.Drawing.Size(75, 23);
+            this.sin.Size = new System.Drawing.Size(124, 69);
             this.sin.TabIndex = 13;
             this.sin.Text = "sin";
             this.sin.UseVisualStyleBackColor = true;
@@ -178,9 +190,10 @@ namespace Calculator
             // 
             // cos
             // 
-            this.cos.Location = new System.Drawing.Point(466, 273);
+            this.cos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cos.Location = new System.Drawing.Point(406, 242);
             this.cos.Name = "cos";
-            this.cos.Size = new System.Drawing.Size(75, 23);
+            this.cos.Size = new System.Drawing.Size(124, 69);
             this.cos.TabIndex = 14;
             this.cos.Text = "cos";
             this.cos.UseVisualStyleBackColor = true;
@@ -188,31 +201,32 @@ namespace Calculator
             // 
             // button_Binary
             // 
-            this.button_Binary.Font = new System.Drawing.Font("宋体", 7F);
-            this.button_Binary.Location = new System.Drawing.Point(64, 273);
+            this.button_Binary.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button_Binary.Location = new System.Drawing.Point(46, 242);
             this.button_Binary.Name = "button_Binary";
-            this.button_Binary.Size = new System.Drawing.Size(75, 23);
+            this.button_Binary.Size = new System.Drawing.Size(124, 69);
             this.button_Binary.TabIndex = 15;
-            this.button_Binary.Text = "十转二";
+            this.button_Binary.Text = "十进制转二进制";
             this.button_Binary.UseVisualStyleBackColor = true;
             this.button_Binary.Click += new System.EventHandler(this.button_Binary_Click);
             // 
             // button_hex
             // 
-            this.button_hex.Font = new System.Drawing.Font("宋体", 7F);
-            this.button_hex.Location = new System.Drawing.Point(197, 273);
+            this.button_hex.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button_hex.Location = new System.Drawing.Point(166, 242);
             this.button_hex.Name = "button_hex";
-            this.button_hex.Size = new System.Drawing.Size(75, 23);
+            this.button_hex.Size = new System.Drawing.Size(124, 69);
             this.button_hex.TabIndex = 16;
-            this.button_hex.Text = "十转十六";
+            this.button_hex.Text = "十进制转十六进制";
             this.button_hex.UseVisualStyleBackColor = true;
             this.button_hex.Click += new System.EventHandler(this.button_hex_Click);
             // 
             // button_Ln
             // 
-            this.button_Ln.Location = new System.Drawing.Point(466, 212);
+            this.button_Ln.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Ln.Location = new System.Drawing.Point(406, 178);
             this.button_Ln.Name = "button_Ln";
-            this.button_Ln.Size = new System.Drawing.Size(75, 23);
+            this.button_Ln.Size = new System.Drawing.Size(124, 69);
             this.button_Ln.TabIndex = 17;
             this.button_Ln.Text = "ln";
             this.button_Ln.UseVisualStyleBackColor = true;
@@ -222,7 +236,7 @@ namespace Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(588, 455);
             this.Controls.Add(this.button_Ln);
             this.Controls.Add(this.button_hex);
             this.Controls.Add(this.button_Binary);
